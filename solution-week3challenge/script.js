c1ccbfc8964156ce19ef9f58717e8bd87d0c5f5f
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-14 00:54:01 +0300

    creates a fetch get request

--- solution-week3challenge/script.js
+++ solution-week3challenge/script.js
@@ -1,28 +1,35 @@
-console.log("test")
-function getAllFirms(){
-  fetch()
-  .then((res)=>)
-}
+//console.log("test")
+document.addEventListener('DOMContentLoaded', function(){
+  initialized()
+})
 
-function renderOneMovie(movie){
-  //build a movie
-const ul = document.getElementById('films')
-const li = document.createElement('li')
-li.classList.add('film-item')
-li.innerHTML=`
-<img src="${film.poster}">
-<div  class="content">
-<h4> ${film.title} </h4>
-<p> <span class = "showtime">${film.showtime}</span> showtime
-</p>
-<p> ${tickets_sold} </p>
-</div>
-`
-ul.appendChild(li)
+//gets all firms resourses
+function getAllFirms(){
+  fetch('http://localhost:3000/films')
+  .then((res)=> res.json())
+  .then((data)=> console.log(data))
 }
 
 //gets data and renders the movies to the DOM
-// function initialized(){
-// filmsdata.forEach((movie)=> renderOneMovie(movie))
+function initialized(){
+  getAllFirms()
+//films.forEach((movie)=> renderOneMovie(movie))
+}
+
+// function renderOneMovie(movie){
+// build a movie
+// const ul = document.getElementById('films')
+// const li = document.createElement('li')
+// li.classList.add('film-item')
+// li.innerHTML=`
+// <img src="${film.poster}">
+// <div  class="content">
+// <h4> ${film.title} </h4>
+// <p> <span class = "showtime">${film.showtime}</span> showtime
+// </p>
+// <p> ${tickets_sold} </p>
+// </div>
+// `
+// ul.appendChild(li)
 // }
-// initialized()
+
