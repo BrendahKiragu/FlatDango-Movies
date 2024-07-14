1d250846f6b06c0846ec9f36b88c4420940bf2ae
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-14 10:56:33 +0300

    fetches data from db.json

--- solution-week3challenge/script.js
+++ solution-week3challenge/script.js
@@ -1,35 +1,22 @@
-//console.log("test")
-document.addEventListener('DOMContentLoaded', function(){
-  initialized()
-})
-
-//gets all firms resourses
-function getAllFirms(){
+// function to get all films resources
+function getAllFilms() {
   fetch('http://localhost:3000/films')
-  .then((res)=> res.json())
-  .then((data)=> console.log(data))
+    .then((res) => res.json())
+    .then((films) => {
+      films.forEach((film) => {
+        renderOneMovie(film);
+      });
+    })
+    .catch(error => console.log('Failed to fetch data:', error));
 }
 
-//gets data and renders the movies to the DOM
-function initialized(){
-  getAllFirms()
-//films.forEach((movie)=> renderOneMovie(movie))
+// function to get data from the fecth and render it to the DOM
+function initialized() {
+ 
 }
 
-// function renderOneMovie(movie){
-// build a movie
-// const ul = document.getElementById('films')
-// const li = document.createElement('li')
-// li.classList.add('film-item')
-// li.innerHTML=`
-// <img src="${film.poster}">
-// <div  class="content">
-// <h4> ${film.title} </h4>
-// <p> <span class = "showtime">${film.showtime}</span> showtime
-// </p>
-// <p> ${tickets_sold} </p>
-// </div>
-// `
-// ul.appendChild(li)
-// }
 
+// Initialize the application
+// document.addEventListener('DOMContentLoaded', function() {
+//   initialized();
+// });
