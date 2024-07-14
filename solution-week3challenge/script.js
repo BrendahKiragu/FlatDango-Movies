a03a358a87f08f46b8d5cc8e1275eaf650c8d4bb
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-14 11:22:16 +0300

    adds a buy ticket button

--- solution-week3challenge/script.js
+++ solution-week3challenge/script.js
@@ -1,6 +1,6 @@
 // function to get all films resources
 function getAllFilms() {
-  fetch('http://localhost:3000/films')
+  fetch('http://localhost:3000/films/')
     .then((res) => res.json())
     .then((films) => {
       films.forEach((film) => {
@@ -10,35 +10,56 @@ function getAllFilms() {
     .catch(error => console.log('Failed to fetch data:', error));
 }
 
-function render(film){
+function renderOneMovie(film){
   const ul = document.getElementById('films')
   const li = document.createElement('li')
-  li.classList('film-item')
+  li.classList.add('film-item')
 
   li.innerHTML =`
   <img src="${film.poster}">
   <div class = "content">
   <h3>${film.title}</h3>
-  <p ${film.showtime}> Showtime</p>
+  <p > Runtime: ${film.runtime}</p>
+  <p > Showtime: ${film.showtime}</p>
   <p> Tickets available: ${film.capacity- film.tickets_sold}</P>
   <button id="buyTicket-${film.id}"disabled>Buy Ticket</button>
   </div>`
   ul.appendChild(li)
 
+const buyTicketBtn = li.querySelector("buyTicket-${film.id}")
+buyTicketBtn.addEventListener("click", function(){
+   if (film.capacity > film.tickets_sold) {
+    film.tickets_sold++;
+     ticketsCountElement.textContent = `Tickets Available: ${film.capacity - film.tickets_sold}`;
+ if (film.tickets_sold >= film.capacity) {
+      buyTicketBtn.disabled = true;
+      buyTicketBtn.textContent = 'Sold Out';
+    }
+  } else {
+    alert('Sorry, no tickets available.');
+  }
+})
 
-
+buyTicketBtn.addEventListener('keydown', (event) => {
+  // Check if the key pressed is Enter or Spacebar
+  if (event.key === 'Enter' || event.key === ' ') {
+    event.preventDefault(); // Prevent default form submission behavior
+    // Trigger the ticket purchase logic
+    buyTicketBtn.click();
+  }
+});
 }
 
 
 
 
-// function to get data from the fecth and render it to the DOM
-// function initialized() {
-//  getAllFilms()
-// }
+//function to get data from the fecth and render it to the DOM
+function initialized() {
+ getAllFilms()
+ }
 
 
-// Initialize the application
-// document.addEventListener('DOMContentLoaded', function() {
-//   initialized();
-// });
+//Initialize the application
+document.addEventListener('DOMContentLoaded', function() {
+  initialized();
+});
