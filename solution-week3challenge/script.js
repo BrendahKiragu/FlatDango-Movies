952d96e4000e71bbfbbbeea6c5f1f6e04db9be4f
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-14 11:05:31 +0300

    creates a list element to hold each film details

--- solution-week3challenge/script.js
+++ solution-week3challenge/script.js
@@ -10,10 +10,32 @@ function getAllFilms() {
     .catch(error => console.log('Failed to fetch data:', error));
 }
 
-// function to get data from the fecth and render it to the DOM
-function initialized() {
- 
+function render(film){
+  const ul = document.getElementById('films')
+  const li = document.createElement('li')
+  li.classList('film-item')
+
+  li.innerHTML =`
+  <img src="${film.poster}">
+  <div class = "content">
+  <h3>${film.title}</h3>
+  <p ${film.showtime}> Showtime</p>
+  <p> Tickets available: ${film.capacity- film.tickets_sold}</P>
+  <button id="buyTicket-${film.id}"disabled>Buy Ticket</button>
+  </div>`
+  ul.appendChild(li)
+
+
+
 }
+
+
+
+
+// function to get data from the fecth and render it to the DOM
+// function initialized() {
+//  getAllFilms()
+// }
 
 
 // Initialize the application
